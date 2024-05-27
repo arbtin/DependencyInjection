8aa9a54468d8b25c93467bc1b247b182df8b9a76
arbtin <arbtin@gmail.com>
2024-05-27 17:53:07 -0500

    Game is in a working state, not in a testable state. Next step is to make it testable!

--- src/main/java/org/example/GameManager.java
+++ src/main/java/org/example/GameManager.java
@@ -1,6 +1,64 @@
 package org.example;
 
+import java.awt.*;
+import java.util.Random;
+import java.util.Scanner;
+
 public class GameManager {
+
+    Random random_number = new Random();
+
+    public RoundResult PlayRound() {
+        Choice player_one;
+        do {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter Choice: (R)ock, (P)aper, or (S)cissors: ");
+            String input = scanner.nextLine().toUpperCase();
+
+            if(input.equals("R")) {
+                player_one = Choice.Rock;
+                break;
+            }
+            else if (input.equals("P")) {
+                player_one = Choice.Paper;
+                break;
+            }
+            else if (input.equals("S")) {
+                player_one = Choice.Scissors;
+                break;
+            }
+            else {
+                System.out.println("Choice not recognized, please try again.");
+            }
+        } while (true);
+
+        Choice player_two = Choice.values()[new Random().nextInt(Choice.values().length)];
+        System.out.println("Player 2 picked " + player_two.toString());
+
+        if(player_one == player_two) {
+            return RoundResult.Draw;
+        }
+        if(player_one == Choice.Rock && player_two == Choice.Scissors ||
+                player_one == Choice.Paper && player_two == Choice.Rock ||
+                player_one == Choice.Scissors && player_two == Choice.Paper) {
+            return RoundResult.PlayerOneWin;
+        }
+        return RoundResult.PlayerTwoWin;
+    }
+
+    enum Choice
+    {
+        Rock,
+        Paper,
+        Scissors
+    }
+
+    enum RoundResult
+    {
+        PlayerOneWin,
+        PlayerTwoWin,
+        Draw
+    }
 
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,33 @@
 package org.example;
 
+import com.sun.management.GarbageCollectionNotificationInfo;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.print("Hello and welcome to Rock, Paper, Scissors!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        GameManager gameManager = new GameManager();
+        Scanner scanner = new Scanner(System.in);
+        do {
+            GameManager.RoundResult result = gameManager.PlayRound();
+
+            if(result == GameManager.RoundResult.PlayerOneWin) {
+                System.out.println("Player one wins!");
+            }
+            else if (result == GameManager.RoundResult.PlayerTwoWin) {
+                System.out.println("Player two wins!");
+            }
+            else {
+                System.out.println("Game is a draw!");
+            }
+
+            System.out.println("Player again (Y/N)? ");
+        } while (scanner.nextLine().toUpperCase().equals("Y"));
     }
 }
